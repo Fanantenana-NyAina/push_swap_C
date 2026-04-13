a69b808ef0101c8ceed7fc5d3f35b69730994d5c
Fanantenana Ny Aina Rakotoarison <fananrak@c1r6s16.42antananarivo.mg>
2026-04-13 10:37:54 +0300

    chore: rm ft_atoi.c and continue the atoi implementation inside the init methode in parsing.c

--- parsing.c
+++ parsing.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsirakot <tsirakot@student.42antananari    +#+  +:+       +#+        */
+/*   By: fananrak <fananrak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 12:28:09 by tsirakot          #+#    #+#             */
-/*   Updated: 2026/03/07 04:07:02 by tsirakot         ###   ########.fr       */
+/*   Updated: 2026/04/13 10:36:48 by fananrak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <limits.h> // need to be in our .h file
 
 int	ft_is_number(char *str)
 {
@@ -43,8 +44,31 @@ int	ft_is_duplicate(t_node *stack, int n)
 	return (0);
 }
 
-long	ft_atoi_ps(char *str)
+long ft_atoi_ps(char *str)
 {
+	long	result;
+	int		sign;
+
+	result = 0;
+	sign = 1;
+	while (*str && (*str == ' ' || (*str >= '\t' && *str <= '\r')))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		if (sign == 1 && result > (long)INT_MAX)
+			return (INT_MAX);
+		if (sign == -1 && - result < (long)INT_MIN)
+			return (INT_MIN);
+		str++;
+	}
+	return (sign * result);
 }
 
 void	ft_parse_args(t_node **stack_a, char **av)

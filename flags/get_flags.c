d772dcfcd34bb7f89d58da302554b7f1e16c3785
Fanantenana Ny Aina Rakotoarison <fananrak@c1r13s2.42antananarivo.mg>
2026-04-16 06:10:06 +0300

    feat(flag): flag utils and main flag choice

--- flags/get_flags.c
+++ flags/get_flags.c
@@ -6,13 +6,50 @@
 /*   By: fananrak <fananrak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 02:20:05 by fananrak          #+#    #+#             */
-/*   Updated: 2026/04/16 02:21:38 by fananrak         ###   ########.fr       */
+/*   Updated: 2026/04/16 06:07:02 by fananrak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "flags.h"
 #include "libft.h"
 
-int	get_flag(char **argv)
+int	is_flag(char *str)
 {
-    
+	if (!ft_strncmp(str, "--simple", 8))
+		return (1);
+	if (!ft_strncmp(str, "--medium", 8))
+		return (1);
+	if (!ft_strncmp(str, "--complex", 9))
+		return (1);
+	if (!ft_strncmp(str, "--adaptive", 10))
+		return (1);
+	if (!ft_strncmp(str, "--bench", 7))
+		return (1);
+	return (0);
+}
+
+t_flag	get_flag(char **argv)
+{
+	int		i;
+	t_flag	flag;
+
+	i = 1;
+	flag = NONE;
+	while (argv[i])
+	{
+		if (!ft_strncmp(argv[i], "--simple", 8))
+			flag = SIMPLE;
+		else if (!ft_strncmp(argv[i], "--medium", 8))
+			flag = MEDIUM;
+		else if (!ft_strncmp(argv[i], "--complex", 9))
+			flag = COMPLEX;
+		else if (!ft_strncmp(argv[i], "--adaptive", 10))
+			flag = ADAPTIVE;
+		else if (!ft_strncmp(argv[i], "--bench", 7))
+			flag = BENCH;
+		i++;
+	}
+	if (flag == NONE)
+		flag = ADAPTIVE;
+	return (flag);
 }
--- src/main.c
+++ src/main.c
@@ -6,11 +6,32 @@
 /*   By: fananrak <fananrak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 22:53:52 by fananrak          #+#    #+#             */
-/*   Updated: 2026/04/15 22:59:08 by fananrak         ###   ########.fr       */
+/*   Updated: 2026/04/16 06:07:51 by fananrak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
+#include "push_swap.h"
+#include "flags.h"
+
+int	main(int argc, char **argv)
 {
-    
+	t_stack	*a;
+	t_flag	flag;
+
+	a = NULL;
+	if (argc < 2)
+		return (0);
+	flag = get_flag(argv);
+	ft_parse_args(&a, argv);
+	if (is_sorted(a))
+		return (0);
+	if (flag == SIMPLE)
+		simple_sort(&a);
+	else if (flag == MEDIUM)
+		medium_sort(&a);
+	else if (flag == COMPLEX)
+		complex_sort(&a);
+	else
+		adaptive_sort(&a);
+	return (0);
 }
